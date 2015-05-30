2809b85983675047b6264d131b1e4f272ba3d864
Forrest Tanaka <forrest@forrest-tanaka.com>
2015-05-29 17:20:30 -0700

    Completed first working version

--- get_disease.js
+++ get_disease.js
@@ -29,49 +29,61 @@ if (!xmlData) {
 
 // Convert from XML to JSON
 var data = xml2json.toJson(xmlData, {object: true});
-//console.log("%o", data.JDBOR.DisorderList.Disorder);
 
+// Connect to the Spindle database and write the data
 MongoClient.connect(url, function(err, db) {
     if (err === null) {
+        var coll = db.collection('Disease2');
+        var bulk = coll.initializeUnorderedBulkOp();
         data.JDBOR.DisorderList.Disorder.forEach(function(disorder) {
             var disease = {};
             disease.ORDOID = disorder.OrphaNumber;
             disease.FullName = disorder.Name.$t;
+            disease.Type = disorder.DisorderType.Name.$t;
+            disease.Active = "Yes";
 
             // Build array of synonyms
             if (disorder.SynonymList.count > 1) {
+                // More than one synonym; pull each array item
                 disease.Synonym = disorder.SynonymList.Synonym.map(function(synonym) {
                     return synonym.$t;
                 });
             } else if (disorder.SynonymList.count === 1) {
+                // Only one synonym; convert single object to single-item array
                 disease.Synonym = [disorder.SynonymList.Synonym.$t];
             }
 
             // Build array of OMIMID
             if (disorder.ExternalReferenceList.count > 1) {
-                disease.OMIMID = _(disorder.ExternalReferenceList.ExternalReference).filter(function(ref) {
+                // More than one external reference; pull each array item with a source of OMIM
+                var omimids = _(disorder.ExternalReferenceList.ExternalReference).filter(function(ref) {
                     return ref.Source === 'OMIM';
                 }).map(function(ref) {
                     return ref.Reference;
                 });
+                if (omimids && omimids.length) {
+                    disease.OMIMID = omimids;
+                }
             } else if (disorder.ExternalReferenceList.count === 1) {
+                // Only one external reference; convert item to array if source is OMIM
                 if (disorder.ExternalReferenceList.ExternalReference.Source === 'OMIM') {
                     disease.OMIMID = [disorder.ExternalReferenceList.ExternalReference.Reference];
                 }
             }
 
-            disease.Type = disorder.DisorderType.Name.$t;
+            bulk.insert(disease);
+        });
 
-            console.dir(disease);
-//            db.collection('Disease').insertOne(
-//                {"name": "Juni"},
-//                function(err, results) {
-//                    console.log(results);
-//                    callback();
-//                }
-//            );
+        // Write the queued documents to the database.
+        bulk.execute(function(err, res) {
+            if (err) {
+                console.log('ERROR: ' + err);
+            } else {
+                console.log('Completed writing collection successfully');
+            }
+            db.close();
         });
     } else {
-        console.log(err);
+        console.log('Could not connect to database: ' + err);
     }
 });
