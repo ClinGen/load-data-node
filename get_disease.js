278c40e0f3d70cce8ac29035fa6a2e04977fd11e
Forrest Tanaka <forrest@forrest-tanaka.com>
2015-06-01 11:34:48 -0700

    Implement promises

--- get_disease.js
+++ get_disease.js
@@ -5,7 +5,7 @@ var xml2json = require('xml2json');
 var util = require('util');
 var _ = require('underscore');
 var Promise = require('bluebird');
-var MongoClient = Promise.promisify(require('mongodb').MongoClient);
+var MongoClient = Promise.promisifyAll(require('mongodb').MongoClient);
 
 var url = 'mongodb://localhost:27017/Spindle';
 
@@ -16,7 +16,7 @@ if (!path) {
     return;
 }
 
-// Read from the specified file tin xmlData
+// Read from the specified file in xmlData
 try {
     var xmlData = fs.readFileSync(path, {encoding: 'UTF-8'});
 } catch(err) {
@@ -30,10 +30,17 @@ if (!xmlData) {
 
 // Convert from XML to JSON in data
 var data = xml2json.toJson(xmlData, {object: true});
+if (!data) {
+    util.log('Couldn’t convert source file ' + path);
+}
 
+// Translate and insert the JSON data into the Mongo DB.
 MongoClient.connectAsync(url).then(function(db) {
     var coll = db.collection('Disease2');
-    var bulk = coll.initializeUnorderedBulkOp();
+    var bulk = Promise.promisifyAll(coll.initializeUnorderedBulkOp());
+    console.log(bulk);
+
+    // Convert each disorder in the given source file to a Mongo bulk entry
     data.JDBOR.DisorderList.Disorder.forEach(function(disorder) {
         var disease = {};
         disease.ORDOID = disorder.OrphaNumber;
@@ -72,68 +79,14 @@ MongoClient.connectAsync(url).then(function(db) {
 
         bulk.insert(disease);
     });
-    return bulk;
-}).then(function(bulk) {
-    // Write the queued documents to the database.
+
+    // We now have all the JSON data into a bulk entry. Write it to the DB
     bulk.executeAsync().then(function() {
         db.close();
-    }
-});
+    }).catch(function(e) {
+        console.error('Error writing to database: ' + e);
+    });
 
-// Connect to the Spindle database and write the data
-MongoClient.connect(url, function(err, db) {
-    if (err === null) {
-        var coll = db.collection('Disease2');
-        var bulk = coll.initializeUnorderedBulkOp();
-        data.JDBOR.DisorderList.Disorder.forEach(function(disorder) {
-            var disease = {};
-            disease.ORDOID = disorder.OrphaNumber;
-            disease.FullName = disorder.Name.$t;
-            disease.Type = disorder.DisorderType.Name.$t;
-            disease.Active = "Yes";
-
-            // Build array of synonyms
-            if (disorder.SynonymList.count > 1) {
-                // More than one synonym; pull each array item
-                disease.Synonym = disorder.SynonymList.Synonym.map(function(synonym) {
-                    return synonym.$t;
-                });
-            } else if (disorder.SynonymList.count === 1) {
-                // Only one synonym; convert single object to single-item array
-                disease.Synonym = [disorder.SynonymList.Synonym.$t];
-            }
-
-            // Build array of OMIMID
-            if (disorder.ExternalReferenceList.count > 1) {
-                // More than one external reference; pull each array item with a source of OMIM
-                var omimids = _(disorder.ExternalReferenceList.ExternalReference).filter(function(ref) {
-                    return ref.Source === 'OMIM';
-                }).map(function(ref) {
-                    return ref.Reference;
-                });
-                if (omimids && omimids.length) {
-                    disease.OMIMID = omimids;
-                }
-            } else if (disorder.ExternalReferenceList.count === 1) {
-                // Only one external reference; convert item to array if source is OMIM
-                if (disorder.ExternalReferenceList.ExternalReference.Source === 'OMIM') {
-                    disease.OMIMID = [disorder.ExternalReferenceList.ExternalReference.Reference];
-                }
-            }
-
-            bulk.insert(disease);
-        });
-
-        // Write the queued documents to the database.
-        bulk.execute(function(err, res) {
-            if (err) {
-                console.log('ERROR: ' + err);
-            } else {
-                console.log('Completed writing collection successfully');
-            }
-            db.close();
-        });
-    } else {
-        console.log('Could not connect to database: ' + err);
-    }
+}).catch(function(e) {
+    console.error('Error opening database: ' + e);
 });
